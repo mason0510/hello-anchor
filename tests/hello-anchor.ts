5ceb90e107829386b44f2c777220a14f1f9a1e30
Yoseph Kurnia Soenggoro <yosephks@Yosephs-MacBook-Pro.local>
2022-02-15 15:29:47 +0400

    initial commit

---
+++ tests/hello-anchor.ts
@@ -0,0 +1,17 @@
+// const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+// const { SystemProgram } = anchor.web3;
+
+describe("Hello Anchor!", () => {
+  anchor.setProvider(anchor.Provider.local());
+
+  it("Uses the workspace to invoke the initialize instruction", async () => {
+    // #region code
+    // Read the deployed program from the workspace.
+    const program = anchor.workspace.HelloAnchor;
+    console.log(program);
+    // Execute the RPC.
+    await program.rpc.initialize();
+    // #endregion code
+  });
+});
